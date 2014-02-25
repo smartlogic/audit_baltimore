cd5424164e2cde2ffab4e3bc36e66d0a4b55b998
Eric Oestrich <eric@oestrich.org>
2014-02-25 12:44:37 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuditBaltimore::Application.config.secret_key_base = '1637745c3583a09dcb0cb0ac62078f8238c14c0024e10fe0e08520bc71bc246e88d4ef115d45c7476a4cf5492f66ddd47a17ea73d50a483c07028e479dca0777'
